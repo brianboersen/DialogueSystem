3ebc74e3b24f2d9c0faf0d8196ea9f2fa677de54
brianboersen <19965@ma-web.nl>
2017-10-11 20:29:34 +0200

    game

---
+++ dialogue_System/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    [SerializeField]
+    private float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        HandleMovement(horizontal);
+	}
+
+    private void HandleMovement(float horizontal)
+    {
+        rb2d.velocity = new Vector2(horizontal * speed, rb2d.velocity.y);
+    }
+}
